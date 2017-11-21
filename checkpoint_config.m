c3e9b04cbff0da59bb155db4f1a40f67def7f129
Jacob <jacob.beallor@queensu.ca>
2017-11-21 13:52:35 -0500

    checkpoint config

---
+++ checkpoint_config.m
@@ -0,0 +1,20 @@
+map_data = xlsread('marathon_mapping_points.xlsx');
+
+x = map_data(:,1);
+y = map_data(:,2);
+
+hold on;
+plot(x,y, 'b-','LineWidth', 1.5)
+
+length_lin = 0;
+for k = 2:length(x)
+    length_lin = length_lin + sqrt((x(k)-x(k-1))^2 + (y(k)-y(k-1))^2);
+end
+
+km_per_unit = 38.57/(sqrt(x(end)^2 + y(end)^2));
+km_length = length_lin * km_per_unit;
+
+z = 0;
+
+checkpoint_location = marathon_mapping(z, x, y, length_lin);
+plot(checkpoint_location(:,1), checkpoint_location(:,2), 'c*', 'MarkerSize', 15);
